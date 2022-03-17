6922c692408232eda95ebd1fd0cef9ca2c801c40
Dima Sivov <dsivov@gmail.com>
2022-03-17 15:04:39 +0000

    encode text and video in microservice

--- models/clip_ms/clip_ms.py
+++ models/clip_ms/clip_ms.py
@@ -1,7 +1,6 @@
 
+from pyexpat import model
 import sys
-import numpy as np
-from pathlib import Path
 sys.path.insert(0, "/notebooks/nebula3_database")
 sys.path.insert(0, "/notebooks")
 from vlm.clip_api import CLIP_API
@@ -13,20 +12,41 @@ class Model(object):
         return {
             'signature_name': 'serving_default',
             'inputs': {
-                'image_data': {
-                    'dtype': 'float'
-                }
+                'text': {
+                    'dtype': 'str'
+                },
+                'movie_id': {
+                    'dtype': 'str'
+                },
+                'scene_element': {
+                    'dtype': 'int'
+                },
             },
             'outputs': {
-                'class_probabilities': {
-                    'dtype': 'float'
+                'vector': {
+                    'dtype': 'str'
                 }
             }
         }
 
     @classmethod
     def predict(cls, data):
-        models = cls.clip.available_models()
+        models = cls.clip.clip_model.available_models()
         return ({'clip_data': data,
                   'clip_models': models   
+                })
+
+    @classmethod
+    def encode_text(cls, data):
+        #models = data
+        vectors = cls.clip.clip_encode_text(data['text'])
+        return ({'clip_data': data,
+                  'clip_vector': vectors   
+                })
+    
+    @classmethod
+    def encode_video(cls, data):
+        vectors = cls.clip.clip_encode_video(data['movie_id'], data['scene_element'])
+        return ({'clip_data': data,
+                  'clip_models': vectors   
                 })
