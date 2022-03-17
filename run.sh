97d461c39632fa2921c2bdc851fc747aa1c484cc
Dima Sivov <dsivov@gmail.com>
2022-03-17 12:01:02 +0200

    run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-source activate nebula_database 
+source activate nebula_vlm 
 export SHELL=/bin/bash
 rm -rf /storage/lost+found
-export PYTHONPATH=/notebooks/
+export PYTHONPATH=/notebooks/:/notebooks/nebula3_database
+cd /notebooks && git clone https://github.com/dsivov/nebula3_vlm.git . &&  git submodule init && git submodule update 
 chmod -R a+w /notebooks
 jupyter lab --ip=0.0.0.0 --no-browser --allow-root --LabApp.trust_xheaders=True --LabApp.disable_check_xsrf=False --LabApp.allow_remote_access=True --LabApp.allow_origin='*'
