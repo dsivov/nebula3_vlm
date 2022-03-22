027bc47a103edd59cb438337c6473593a3e7ba5e
Dima Sivov <dimas@gmail.co>
2022-03-22 10:49:31 +0000

    chanf run to handle submodule

--- run.sh
+++ run.sh
@@ -3,6 +3,8 @@ source activate nebula_vlm
 export SHELL=/bin/bash
 rm -rf /storage/lost+found
 export PYTHONPATH=/notebooks/:/notebooks/nebula3_database
-cd /notebooks && git clone https://github.com/dsivov/nebula3_vlm.git . &&  git submodule init && git submodule update 
+cd /notebooks && git clone https://github.com/dsivov/nebula3_vlm.git .   
+cd /notebooks && git submodule init && git submodule update
+cd /notebooks/nebula3_database && git checkout main  
 chmod -R a+w /notebooks
-jupyter lab --ip=0.0.0.0 --no-browser --allow-root --LabApp.trust_xheaders=True --LabApp.disable_check_xsrf=False --LabApp.allow_remote_access=True --LabApp.allow_origin='*'
+#jupyter lab --ip=0.0.0.0 --no-browser --allow-root --LabApp.trust_xheaders=True --LabApp.disable_check_xsrf=False --LabApp.allow_remote_access=True --LabApp.allow_origin='*'
